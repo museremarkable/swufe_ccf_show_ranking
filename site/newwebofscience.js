1b22253924514f6b262ac541316f5c8404a925b4
Nixiak_nan <609823875@qq.com>
2021-10-07 12:04:43 +0800

    修复了web of science上不能正常显示的Bug

--- site/newwebofscience.js
+++ site/newwebofscience.js
@@ -13,11 +13,10 @@ newwebofscience.addRankings = function() {
 	results.each(function(index) {
 
 		let result = $(this);
-		swufe_list = result.find("span[class^='swufe']");
+		swufe_list = result.find("span[class$='none']");
 		if (swufe_list.length == 0) {
 			let source = result.find("span[lang='en']");
 			let name = source.text().trim();
-
 			if (name.length != 0) {
 				for (let getRankingSpan of newwebofscience.rankSpanListSwufe) {
 					source.after(getRankingSpan(name.toUpperCase()));
@@ -31,6 +30,23 @@ newwebofscience.addRankings = function() {
 					}
 				}
 			}
+			
+			let source_cn = result.find("span[lang='zh-cn']");
+			let name_cn = source_cn.text().trim();
+			if (name_cn.length != 0) {
+				for (let getRankingSpan of newwebofscience.rankSpanListSwufe) {
+					source_cn.after(getRankingSpan(name_cn));
+				}
+			} else {
+				let source_cn2 = result.find("button[lang='zh-cn']");
+				let name_cn2 = source_cn2.text().trim();
+				if (name_cn2.length != 0) {
+					for (let getRankingSpan of newwebofscience.rankSpanListSwufe) {
+						source_cn2.after(getRankingSpan(name_cn2));
+					}
+				}
+			}
+			
 		}
 	});
 }
